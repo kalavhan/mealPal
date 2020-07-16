5acc409b9dde8a2a981a47b10115fdac77d0890e
kalavhan <josue.brigido1@gmail.com>
2020-07-16 01:18:28 -0600

    Fix javascript tests

--- src/tests/action.test.js
+++ src/tests/action.test.js
@@ -1,17 +1,19 @@
 import { getCategories, setCategoryMeals } from '../actions/index';
 
-test('By calling getCategories it should return an object with type property matching GET_CATEGORIES', () => {
-  expect(getCategories([{ id: 1 }, { id: 2 }]).type).toEqual('GET_CATEGORIES');
-});
+describe('Test for the meal details reducer', () => {
+  it('By calling getCategories it should return an object with type property matching GET_CATEGORIES', () => {
+    expect(getCategories([{ id: 1 }, { id: 2 }]).type).toEqual('GET_CATEGORIES');
+  });
 
-test('By calling getCategories it should return an object with categories property matching passed array', () => {
-  expect(getCategories([{ id: 1 }, { id: 2 }]).categories).toEqual([{ id: 1 }, { id: 2 }]);
-});
+  it('By calling getCategories it should return an object with categories property matching passed array', () => {
+    expect(getCategories([{ id: 1 }, { id: 2 }]).categories).toEqual([{ id: 1 }, { id: 2 }]);
+  });
 
-test('By calling setCategoryMeals it should return an object with type property matching SET_CATEGORY_MEALS', () => {
-  expect(setCategoryMeals([{ id: 1 }, { id: 2 }]).type).toEqual('SET_CATEGORY_MEALS');
-});
+  it('By calling setCategoryMeals it should return an object with type property matching SET_CATEGORY_MEALS', () => {
+    expect(setCategoryMeals([{ id: 1 }, { id: 2 }]).type).toEqual('SET_CATEGORY_MEALS');
+  });
 
-test('By calling setCategoryMeals it should return an object with meals property matching passed array', () => {
-  expect(setCategoryMeals([{ id: 1 }, { id: 2 }]).meals).toEqual([{ id: 1 }, { id: 2 }]);
+  it('By calling setCategoryMeals it should return an object with meals property matching passed array', () => {
+    expect(setCategoryMeals([{ id: 1 }, { id: 2 }]).meals).toEqual([{ id: 1 }, { id: 2 }]);
+  });
 });
--- src/tests/categoriesReducer.test.js
+++ src/tests/categoriesReducer.test.js
@@ -1,9 +1,11 @@
 import categoriesReducer from '../reducers/categories';
 
-test('By sending the correct type it should return the categories array', () => {
-  expect(categoriesReducer('', { type: 'GET_CATEGORIES', categories: [{ id: 1 }] })).toEqual([{ id: 1 }]);
-});
+describe('Test for the meal details reducer', () => {
+  it('By sending the correct type it should return the categories array', () => {
+    expect(categoriesReducer('', { type: 'GET_CATEGORIES', categories: [{ id: 1 }] })).toEqual([{ id: 1 }]);
+  });
 
-test('By default without the correct type it should return an empty array', () => {
-  expect(categoriesReducer([], { type: '', categories: [{ id: 1 }] })).toEqual([]);
+  it('By default without the correct type it should return an empty array', () => {
+    expect(categoriesReducer([], { type: '', categories: [{ id: 1 }] })).toEqual([]);
+  });
 });
