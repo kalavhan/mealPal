35915c3daadcffbfa853886b15660d2937593799
kalavhan <josue.brigido1@gmail.com>
2020-07-16 07:34:34 -0600

    Add more tests

--- src/components/MealsCard.js
+++ src/components/MealsCard.js
@@ -5,7 +5,7 @@ const MealsCard = ({ meals, showMealDetails }) => (
   <div className="mealsList">
     <h1>List of meals</h1>
     {meals.map(meal => (
-      <div className="mealCard layer-1" role="button" key={meal.idMeal} onClick={() => showMealDetails(meal.idMeal)} onKeyUp={() => {}} tabIndex={0}>
+      <div className="mealCard layer-1" data-testid={meal.idMeal} role="button" key={meal.idMeal} onClick={() => showMealDetails(meal.idMeal)} onKeyUp={() => {}} tabIndex={0}>
         <img src={meal.strMealThumb} alt="mealImage" />
         <h3>{meal.strMeal}</h3>
       </div>
